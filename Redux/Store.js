3d49cd64f4a077b0dadda59592b7c9bb56b62791
iraklisarkisovi <sarqisovirakli@gmail.com>
2025-10-18 12:59:28 +0400

    feat: redux installed

---
+++ Redux/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./Reducer";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+});
